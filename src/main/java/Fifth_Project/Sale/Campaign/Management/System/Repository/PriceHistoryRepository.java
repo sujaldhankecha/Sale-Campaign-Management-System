5b6690f8ddf4fbf810097f2c0d7af9a92be4f7a0
Sujal Dhankecha <dhakechasujal@gmail.com>
2025-08-07 17:51:31 +0530

    add native query

--- src/main/java/Fifth_Project/Sale/Campaign/Management/System/Repository/PriceHistoryRepository.java
+++ src/main/java/Fifth_Project/Sale/Campaign/Management/System/Repository/PriceHistoryRepository.java
@@ -2,13 +2,17 @@ package Fifth_Project.Sale.Campaign.Management.System.Repository;
 
 import Fifth_Project.Sale.Campaign.Management.System.Model.PriceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
+
+    @Query(value = "SELECT EXISTS (SELECT 1 FROM price_history WHERE product_id = :productId AND reason LIKE %:reason%)", nativeQuery = true)
     boolean existsByProductIdAndReasonContaining(Long productId, String reason);
+
     List<PriceHistory> findByProductId(Long productId);
 }
 
